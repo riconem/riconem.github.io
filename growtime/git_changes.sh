e2f04cbce75ee0e6a10bb24d904514519c46ee78
Enrico Nemitz <enrico_nemitz@hotmail.de>
2020-11-18 19:35:47 +0100

    auto-pi: adding picture of 19:35:47-18.11.20

--- growtime/git_changes.sh
+++ growtime/git_changes.sh
@@ -3,5 +3,5 @@
 cd "$( dirname "$BASH_SOURCE" )"
 
 git add -A
-git commit "auto-pi: adding picture of $(date +%H:%M:%S-%d.%m.%y)"
+git commit -m "auto-pi: adding picture of $(date +%H:%M:%S-%d.%m.%y)"
 git push origin master
