24c9a40ff7af65489a8f808cb3fc371cf89bf1b7
Enrico Nemitz <enrico_nemitz@hotmail.de>
2021-01-10 23:15:03 +0100

    uncomment git push and pull

--- growtime/make_image.py
+++ growtime/make_image.py
@@ -1,11 +1,11 @@
 from git import git_pull, git_change
 from capture import Camera
 
-#git_pull()
+git_pull()
 
 dl = Camera('dl', 'dl_2021_01_09')
 dl.capture_image()
 dl.create_json_from_images()
 dl.add_json_to_js()
 
-# git_change()
+git_change()
